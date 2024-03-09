991b7cd0fc06d5e99330f2ad719e61164aa92b09
fazakaserik <the.fazakas.erik@gmail.com>
2024-03-09 13:37:32 +0100

    MAUI setup

---
+++ client/MoveNetClient/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoveNetClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ client/MoveNetClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoveNetClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
